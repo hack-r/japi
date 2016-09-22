6d1a063b9ce9a533a875894ec4eef34bd4cd7167
Jason Miller <Jason.Miller@laureate.net>
2016-09-22 14:16:18 -0400

    works!

--- src/main/java/com/control2me/japi/SwaggerDocumentSetup.java
+++ src/main/java/com/control2me/japi/SwaggerDocumentSetup.java
@@ -24,8 +24,8 @@ public class SwaggerDocumentSetup extends HttpServlet {
         beanConfig.setDescription( "Our core API's that can be used for all sorts of stuff. Please Enjoy." );
 
         beanConfig.setSchemes(new String[]{"https"});
-//        beanConfig.setHost("localhost:8080");
-        beanConfig.setHost("www.control2me.com");
+        beanConfig.setHost("localhost:8080");
+//        beanConfig.setHost("www.control2me.com");
         beanConfig.setBasePath("/japi/services");
         beanConfig.setResourcePackage( "com.control2me.japi.services" );
         
